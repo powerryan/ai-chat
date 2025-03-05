f448a01859fdc8984ffd4b4ec560e62da20d8653
Ryan Power <ryanpowr@gmail.com>
2025-03-05 01:07:39 -0800

    create app

---
+++ routes.py
@@ -0,0 +1,22 @@
+from flask import render_template, request, redirect, url_for, flash, send_from_directory, jsonify
+from __main__ import app
+import json
+from ollama import Client
+
+ollama_client = Client(host='http://localhost:11434')
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    data = request.get_json()
+    question = data.get('question', '')
+    response = ollama_client.generate(model='mistral', prompt=question)
+    #return render_template("index.html", answer=response)
+    return {"answer": f"{response.response}"}, 200
+
+@app.route('/home', methods=['POST', 'GET'])
+def homePage():
+    return render_template("index.html")
+
+@app.route('/', methods=['POST', 'GET'])
+def landing():
+    return render_template("index.html")
